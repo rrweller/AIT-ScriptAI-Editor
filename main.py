11a63e90ab594821006216b11381304d3e5aabcb
unknown <riley.r.weller@gmail.com>
2023-12-24 15:54:52 +0100

    Reverted editing code and fixed viewport on line loading

--- main.py
+++ main.py
@@ -3,68 +3,57 @@ import json
 from tkinter import filedialog, messagebox
 from json_parser import read_scriptai_json
 from view import Viewport
-from line_edit import LineEditor
 
-class ScriptAILineEditorApp:
+class LineEditorApp:
     def __init__(self, master):
         self.master = master
-        self.master.title("Autobeam ScriptAI Line Editor")
-        
-        # Canvas frame for displaying lines
-        self.canvas_frame = tk.Frame(master)
-        self.canvas_frame.pack(side="left", fill="both", expand=True)
-        
-        # Side panel frame for buttons and settings
-        self.side_panel = tk.Frame(master, bg="lightgrey", width=200)
-        self.side_panel.pack(side="right", fill="y", expand=False)
-        
-        # Create a canvas on which we will display the lines
-        self.canvas = tk.Canvas(self.canvas_frame, bg="white", width=800, height=600)
-        self.canvas.pack(side="top", fill="both", expand=True)
-        
-        # Button to load a JSON file
-        self.load_button = tk.Button(self.side_panel, text="Load Line JSON", command=self.load_json_dialog)
-        self.load_button.pack(pady=10)
-        
-        # Button to export the JSON File
-        self.export_button = tk.Button(self.side_panel, text="Export JSON", command=self.export_json_dialog)
-        self.export_button.pack(side=tk.BOTTOM, pady=10)
+        self.master.title("ScriptAI Line Editor")
 
-        # Initialize the viewport for handling the canvas view
-        self.viewport = Viewport(self.canvas)
-
-        # Set up the line editor for point editing
-        self.line_editor = LineEditor(self.viewport)
-        
-        # Initialize the path data
-        self.path_type = None
+        self.setup_ui()
         self.path_data = None
 
-    def load_json_dialog(self):
+    def setup_ui(self):
+        self.create_canvas()
+        self.create_side_panel()
+
+    def create_canvas(self):
+        self.canvas_frame = tk.Frame(self.master)
+        self.canvas_frame.pack(side="left", fill="both", expand=True)
+        self.canvas = tk.Canvas(self.canvas_frame, bg="white", width=800, height=600)
+        self.canvas.pack(side="top", fill="both", expand=True)
+        self.viewport = Viewport(self.canvas)
+
+    def create_side_panel(self):
+        self.side_panel = tk.Frame(self.master, bg="lightgrey", width=200)
+        self.side_panel.pack(side="right", fill="y", expand=False)
+        self.load_button = tk.Button(self.side_panel, text="Load JSON", command=self.load_json)
+        self.load_button.pack(pady=10)
+        self.export_button = tk.Button(self.side_panel, text="Export JSON", command=self.export_json)
+        self.export_button.pack(side=tk.BOTTOM, pady=10)
+
+    def load_json(self):
         file_path = filedialog.askopenfilename(filetypes=[("JSON files", "*.json")])
         if file_path:
-            self.load_path_data(file_path)
-    
-    def export_json_dialog(self):
+            self.read_json(file_path)
+
+    def read_json(self, file_path):
+        _, self.path_data = read_scriptai_json(file_path)
+        self.viewport.set_path_data(self.path_data['path'])
+
+    def export_json(self):
         file_path = filedialog.asksaveasfilename(filetypes=[("JSON files", "*.json")], defaultextension=".json")
         if file_path:
-            self.save_edited_path(file_path)
+            self.write_json(file_path)
 
-    def load_path_data(self, file_path):
-        self.path_type, self.path_data = read_scriptai_json(file_path)
-        self.viewport.path_data = self.path_data['path']  # Set the path data for the viewport
-        self.viewport.fit_content(self.path_data['path'])  # Fit the content using the viewport
-
-    def save_edited_path(self, file_path):
+    def write_json(self, file_path):
         try:
             with open(file_path, 'w') as json_file:
                 json.dump(self.path_data, json_file, indent=4)
-            messagebox.showinfo("Success", "The JSON file has been exported successfully.")
+            messagebox.showinfo("Success", "JSON file exported successfully.")
         except Exception as e:
-            messagebox.showerror("Error", f"An error occurred while saving the file: {e}")
+            messagebox.showerror("Error", f"Error saving file: {e}")
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = ScriptAILineEditorApp(root)
-    root.after(100, lambda: root.attributes("-topmost", False))  # Ensure the canvas dimensions are updated
+    app = LineEditorApp(root)
     root.mainloop()
--- view.py
+++ view.py
@@ -3,105 +3,86 @@ import tkinter as tk
 class Viewport:
     def __init__(self, canvas):
         self.canvas = canvas
-        self.zoom_scale = 1.0
+        self.zoom = 1.0
         self.offset_x = 0
         self.offset_y = 0
         self.path_data = []
-        self.item_to_index_map = {}  # Maps canvas item IDs to their corresponding index in path_data
+        self.bind_events()
 
-        # Bind mouse events for zoom and pan interaction
-        self.canvas.bind('<MouseWheel>', self.on_mousewheel)
-        self.canvas.bind('<B3-Motion>', self.on_mouse_drag)
-        self.canvas.bind('<Button-3>', self.on_mouse_click)
+    def bind_events(self):
+        self.canvas.bind('<MouseWheel>', self.zoom_view)
+        self.canvas.bind('<B3-Motion>', self.drag_view)
+        self.canvas.bind('<Button-3>', self.start_drag)
 
-    def on_mousewheel(self, event):
+    def zoom_view(self, event):
         scale_factor = 1.1
-        if event.delta > 0:  # Zoom in
-            self.zoom_scale *= scale_factor
-        elif event.delta < 0:  # Zoom out
-            self.zoom_scale /= scale_factor
+        self.zoom *= scale_factor if event.delta > 0 else 1 / scale_factor
         self.redraw()
 
-    def on_mouse_click(self, event):
+    def start_drag(self, event):
         self.canvas.scan_mark(event.x, event.y)
 
-    def on_mouse_drag(self, event):
-        # Pans the canvas view
+    def drag_view(self, event):
         self.canvas.scan_dragto(event.x, event.y, gain=1)
 
-    def fit_content(self, points):
+    def set_path_data(self, points):
         self.path_data = points
-
-        # Reset the zoom scale and pan offsets
-        self.zoom_scale = 1.0
-        self.offset_x = 0
-        self.offset_y = 0
-
-        # Make sure canvas dimensions are up to date
-        self.canvas.update_idletasks()
-        
-        # Skip fitting if there are no points
-        if not points:
-            return
-        
-        # Calculate bounding box of points
-        min_x = min(point['x'] for point in points)
-        max_x = max(point['x'] for point in points)
-        min_y = min(point['y'] for point in points)
-        max_y = max(point['y'] for point in points)
-        
-        # Determine the canvas size
-        canvas_width = self.canvas.winfo_width()
-        canvas_height = self.canvas.winfo_height()
-        
-        # Calculate the necessary scale to fit all points
-        x_scale = canvas_width / (max_x - min_x) if max_x > min_x else float('inf')
-        y_scale = canvas_height / (max_y - min_y) if max_y > min_y else float('inf')
-        self.zoom_scale = min(x_scale, y_scale) * 0.9  # Use the smaller scale and add padding
-
-        # Calculate the center point of the bounding box
-        center_x = (max_x + min_x) / 2
-        center_y = (max_y + min_y) / 2
-        
-        # Calculate the offset to center the bounding box in the canvas
-        self.offset_x = (canvas_width / 2) - (center_x * self.zoom_scale)
-        self.offset_y = (canvas_height / 2) - (center_y * self.zoom_scale)
-
+        self.reset_view()
         self.redraw()
 
-    def draw_point(self, x, y, radius=3, color='blue', point_index=None):
-        canvas_x = x * self.zoom_scale + self.offset_x
-        canvas_y = y * self.zoom_scale + self.offset_y
-        item = self.canvas.create_oval(
-            canvas_x - radius, canvas_y - radius, 
-            canvas_x + radius, canvas_y + radius,
-            fill=color,
-            tags=('path_point', f'point{point_index}')
-        )
-        return item
+    def reset_view(self):
+        print("Resetting view...")
+        if not self.path_data:
+            return
+        
+        self.canvas.update_idletasks()  # Update the canvas to get the correct dimensions
+        # Reset canvas view position to the initial state
+        self.canvas.xview_moveto(0)
+        self.canvas.yview_moveto(0)
+
+        min_x = min(point['x'] for point in self.path_data)
+        max_x = max(point['x'] for point in self.path_data)
+        min_y = min(point['y'] for point in self.path_data)
+        max_y = max(point['y'] for point in self.path_data)
+
+        canvas_width = self.canvas.winfo_width()
+        canvas_height = self.canvas.winfo_height()
+
+        x_scale = canvas_width / (max_x - min_x) if max_x > min_x else 1
+        y_scale = canvas_height / (max_y - min_y) if max_y > min_y else 1
+        self.zoom = min(x_scale, y_scale) * 0.9
+        print(f"Setting zoom to {self.zoom}")
+
+        # Center the line in the canvas
+        center_x = (max_x + min_x) / 2
+        center_y = (max_y + min_y) / 2
+        print(f"Center of line is at x:{center_x}, y:{center_y}")
+
+        self.offset_x = (canvas_width / 2) - (center_x * self.zoom)
+        self.offset_y = (canvas_height / 2) - (center_y * self.zoom)
+        print(f"Offsetting line to x: {self.offset_x}, y: {self.offset_y}")
 
     def redraw(self):
-        self.canvas.delete('path_point')
-        self.canvas.delete('path_line')
-        self.item_to_index_map.clear()
+        self.canvas.delete("all")
+        if not self.path_data:
+            return
 
-        for i, point in enumerate(self.path_data):
-            item = self.draw_point(point['x'], point['y'], radius=3, color='blue', point_index=i)
-            self.item_to_index_map[item] = i
+        for i in range(len(self.path_data) - 1):
+            self.draw_line(self.path_data[i], self.path_data[i + 1])
+            self.draw_point(self.path_data[i])
 
-        # Redraw all points and lines with the current path data
-        if self.path_data:
-            for idx in range(len(self.path_data) - 1):
-                self.draw_line(self.path_data[idx]['x'], self.path_data[idx]['y'],
-                               self.path_data[idx + 1]['x'], self.path_data[idx + 1]['y'])
+        self.draw_point(self.path_data[-1])
 
-            for point in self.path_data:
-                self.draw_point(point['x'], point['y'])
-        print(self.item_to_index_map)
-    
-    def draw_line(self, x1, y1, x2, y2, color='blue', width=1):
-        canvas_x1 = x1 * self.zoom_scale + self.offset_x
-        canvas_y1 = y1 * self.zoom_scale + self.offset_y
-        canvas_x2 = x2 * self.zoom_scale + self.offset_x
-        canvas_y2 = y2 * self.zoom_scale + self.offset_y
-        self.canvas.create_line(canvas_x1, canvas_y1, canvas_x2, canvas_y2, fill=color, width=width, tags='path_line')
+        self.canvas.update_idletasks()  # Force the canvas to update its layout
+
+    def draw_point(self, point, radius=3, color='blue'):
+        x, y = self.transform(point['x'], point['y'])
+        self.canvas.create_oval(x - radius, y - radius, x + radius, y + radius, fill=color)
+
+    def draw_line(self, start, end, color='blue', width=1):
+        x1, y1 = self.transform(start['x'], start['y'])
+        x2, y2 = self.transform(end['x'], end['y'])
+        self.canvas.create_line(x1, y1, x2, y2, fill=color, width=width)
+
+    def transform(self, x, y):
+        return x * self.zoom + self.offset_x, y * self.zoom + self.offset_y
